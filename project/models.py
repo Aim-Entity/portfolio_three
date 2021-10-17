13905811f2c2f0065ec8597a429982e9c4ac2811
Aim-Entity <59455267+Aim-Entity@users.noreply.github.com>
2021-10-17 17:10:42 +0100

    project page done

--- project/models.py
+++ project/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    link = models.URLField()
+
+    software1 = models.FileField(upload_to="software", blank=True, null=True)
+    software2 = models.FileField(upload_to="software", blank=True, null=True)
+    software3 = models.FileField(upload_to="software", blank=True, null=True)
+
+    s1_name = models.CharField(max_length=300, blank=True, null=True)
+    s2_name = models.CharField(max_length=300, blank=True, null=True)
+    s3_name = models.CharField(max_length=300, blank=True, null=True)
+
+    image = models.ImageField(upload_to="project_image")
+
+    def __str__(self):
+        return self.name
--- project/views.py
+++ project/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Project
+
 
 def project(request):
-  context = {
-    
-  }
-  return render(request, "project/project.html", context)
+    projects = Project.objects.all()
+
+    context = {
+        "projects": projects
+    }
+    return render(request, "project/project.html", context)
